347c1570d1c6432e5dfb0884da093c2933b7df03
Madhuka Hettiarachchi <138968964+MadhukaH@users.noreply.github.com>
2025-02-28 22:27:36 +0530

    Create a temporary profile photo change function

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {useSelector} from 'react-redux'
 import { Button, TextInput } from 'flowbite-react'
+import { use } from 'react';
 
 
 export default function DashProfile() {
   const {currentUser} = useSelector(state => state.user)
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePikerRef = useRef(null);
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if(file){
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+    
+  };
+  
+
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    console.log('uploading image....');
+  };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>profile</h1>
       <form className='flex flex-col gap-4'>
+        <input type='file' accept='image/*' onChange={handleImageChange} ref={filePikerRef} hidden />
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePikerRef.current.click()}>
 
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-
-        <img src={currentUser.profilePicture} alt='user' className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'/>
+        <img src={imageFileUrl || currentUser.profilePicture} 
+        alt='user' className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'/>
 
         </div>
           <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username} />
